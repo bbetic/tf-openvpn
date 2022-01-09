6a2b6555f4da6a3e7f328c9506c4a075bafbc83d
bbetic <seth.brua+gitlab@gmail.com>
2022-01-08 21:50:30 -0600

    add remote config to terraform.tf

--- data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "openvpn" {
-  backend = "http"
-  config = {
-    address  = var.openvpn_remote_state_address
-    username = var.openvpn_username
-    password = var.openvpn_access_token
-  }
-}
--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   backend "http" {
-    
+    address  = var.openvpn_remote_state_address
+    username = var.openvpn_username
+    password = var.openvpn_access_token
+    lock_address = var.openvpn_lock_address ? var.openvpn_lock_address : "${var.openvpn_remote_state_address}/lock"
+    unlock_address = var.openvpn_lock_address ? var.openvpn_lock_address : "${var.openvpn_remote_state_address}/lock"
   }
   experiments = [module_variable_optional_attrs]
 
